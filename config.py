62385f8e8aef050bce4994cf89d086b929050c05
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-04-22 19:43:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 # FSUBS configuration
 FSUBS = [
-    {'_id': ,-1002201872723 "CHANNEL_NAME": "stelleron_Hunter"},
+    {'_id': 1, "CHANNEL_ID": -1002201872723, "CHANNEL_NAME": "stelleron_Hunter"},
 ]
 
 
