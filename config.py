41dfd68cabed6a6ace40f1fd1b83364daec68247
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-04-22 18:25:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,31 +9,31 @@ settings = {
     "AUTO_DEL": True,  # bool write True or False
     "STICKER_ID": "CAACAgUAAyEFAASAgUwqAAJh_mckw2STkeY1WMOHJGY4Hs9_1-2fAAIPFAACYLShVon-N6AFLnIiHgQ",
     "stk_del_timer": 1, # in seconds
-    "bot_admin": [2093552866] #e.g. 1963929292,38739292827 differetiate admins with a comma
+    "bot_admin": [7654385403] #e.g. 1963929292,38739292827 differetiate admins with a comma
 }
 
 HELP_MSG = """■ 𝗛𝗲𝗹𝗹𝗼, 𝗔𝗱𝗺𝗶𝗻𝘀!\n\n<blockquote expandable><b>ɴᴇᴇᴅ ʜᴇʟᴘ? ɪᴛ’s sɪᴍᴘʟᴇ: ᴊᴜsᴛ ᴋɴᴏᴄᴋ ᴏɴ <i>Sᴀɪ’s</i> ᴅᴏᴏʀ (ᴛʜᴀᴛ’s ᴍᴇ, ʙʏ ᴛʜᴇ ᴡᴀʏ). 🙋‍♂️ ᴡʜᴀᴛᴇᴠᴇʀ ɪᴛ ɪs—ǫᴜᴇsᴛɪᴏɴs, ᴄᴏɴᴄᴇʀɴs, ᴇxɪsᴛᴇɴᴛɪᴀʟ ᴄʀɪsᴇs ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ᴛʜɪs ʙᴏᴛ—ᴊᴜsᴛ ᴀsᴋ.</b></blockquote>\n\n<blockquote expandable><b>ᴡʜʏ ɪs ᴛʜɪs ᴍᴇssᴀɢᴇ sᴏ sʜᴏʀᴛ? ʙᴇᴄᴀᴜsᴇ ᴛʜɪs ʙᴏᴛ ʜᴀs ᴀʟʀᴇᴀᴅʏ ᴄᴏɴsᴜᴍᴇᴅ ᴀ ʀɪᴅɪᴄᴜʟᴏᴜs ᴀᴍᴏᴜɴᴛ ᴏғ ᴍʏ ᴛɪᴍᴇ, ᴀɴᴅ ɪ’ᴍ ɴᴏᴛ ɪɴ ᴛʜᴇ ᴍᴏᴏᴅ ᴛᴏ ᴡʀɪᴛᴇ ᴀ ɴᴏᴠᴇʟ ʜᴇʀᴇ. 🤷‍♂️ sᴏ, ʏᴇᴀʜ, ʀᴇᴀᴄʜ ᴏᴜᴛ, ᴀɴᴅ ɪ’ʟʟ sᴏʀᴛ ɪᴛ ᴏᴜᴛ.</b></blockquote>\n\n<blockquote><b>ɴᴏᴡ ɢᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴛʜᴏsᴇ ʟɪɴᴋs ʟɪᴋᴇ ᴛʜᴇ ʀᴏᴄᴋsᴛᴀʀ ᴀᴅᴍɪɴ ʏᴏᴜ ᴀʀᴇ! 💪</b></blockquote>
 """  # shown only to admins
 
 # Bot token @Botfather
-TG_BOT_TOKEN = '7594941060:AAG7d86EiyUqqw73WuG5FemHgMP3kIYyEvs'
+TG_BOT_TOKEN = ''
 # Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "26684254"))
+APP_ID = int(os.environ.get("APP_ID", ""))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "fc836096a68be3a4fcd7594cb3d9326f")
+API_HASH = os.environ.get("API_HASH", "")
 
 # Your db channel Id
-DB_CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002456061680"))
+DB_CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 # NAME OF OWNER
-OWNER = os.environ.get("OWNER", "Rai Yan")
+OWNER = os.environ.get("OWNER", "Stelleron Hunter")
 
 # OWNER ID
-OWNER_ID = 6161189904
+OWNER_ID = 7654385403
 
 # SUDO: those who can edit admins in channel
-SUDO = [6161189904]
+SUDO = [7654385403]
 if OWNER_ID not in SUDO:
     SUDO.append(OWNER_ID)
 
@@ -41,22 +41,22 @@ if OWNER_ID not in SUDO:
 PORT = os.environ.get("PORT", "8108")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sainallamilli:Filestore1@cluster0.8urcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 # FSUBS configuration
 FSUBS = [
-    {'_id': -1002349345920, "CHANNEL_NAME": "Animes2u"},
+    {'_id': ,-1002201872723 "CHANNEL_NAME": "@telleron_Hunter"},
 ]
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 # Start message
-START_MSG = os.environ.get("START_MESSAGE", "<blockquote><b>ʜᴇʏ ᴛʜᴇʀᴇ, {mention} ✌🏻</b></blockquote>\n\n<blockquote expandable><b>I'ᴍ ᴍɪᴛsᴜʜᴀ ᴍɪʏᴀᴍɪᴢᴜ, ᴀ ʏᴏᴜʀ ᴘᴇʀsᴏɴᴀʟ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ. 📂 I sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ ᴀ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ, ᴀɴᴅ ᴏᴛʜᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ᴛʜᴇᴍ ᴛʜʀᴏᴜɢʜ ᴀ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ. 🖇️ Lɪᴋᴇ ᴍʏ ᴏᴡɴ ᴏʀɢᴀɴɪᴢᴇᴅ ʟɪғᴇ, ɪ'ᴍ ʜᴇʀᴇ ᴛᴏ ᴋᴇᴇᴘ ʏᴏᴜʀ ғɪʟᴇs ɪɴ ᴏʀᴅᴇʀ! ❤️‍🩹</b></blockquote>")
-ADMINS = [6161189904]
+START_MSG = os.environ.get("START_MESSAGE", "<b><blockquote>Hᴇʏ!! {mention} Wᴇʟᴄᴏᴍᴇ Tᴏ Cᴏᴍᴍᴜɴɪᴛʏ. Iғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴜᴘᴘᴏʀᴛ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ, ʏᴏᴜ ᴄᴀɴ ᴅᴏ sᴏ ʙʏ sᴜʙsᴄʀɪʙɪɴɢ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ. \n</b>Jᴏɪɴ ᴏᴜʀ Gʀᴏᴜᴘ ~ <a href=https://t.me/+C30sfk0zGLpkYTBl>Cʟɪᴄᴋ Hᴇʀᴇ </a>\n\nᴀɴɪᴍᴇ ɪɴᴅᴇx ~ <a href=https://t.me/+-JIJhwaFEb4zY2U1>ɪɴᴅᴇx </a>\n\nTʜᴀɴᴋs Fᴏʀ ʏᴏᴜʀ Sᴜᴘᴘᴏʀᴛ</blockquote></b>")
+ADMINS = [7654385403]
 # Add other admin IDs here as needed, ensuring not to include OWNER_ID
-other_admin_ids = [5376373647]  # Replace with actual admin IDs
+other_admin_ids = [5274479443]  # Replace with actual admin IDs
 for admin_id in other_admin_ids:
     if admin_id != OWNER_ID:
         ADMINS.append(admin_id)
